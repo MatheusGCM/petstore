a6b3ff0fd83b417ddbaa450602aba15fbebdb1fe
MatheusGCM <nexteu10@outlook.com>
2022-09-18 21:42:54 -0300

    Otimizando

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import Pets from './components/Pets';
 
 const App = () => {
   const [petsData, setPetsData] = useState();
-  const [orderId, setOrderId] = useState();
-  const [orderType, setOrderType] = useState();
-  const [orderPrice, setOrderPrice] = useState();
+  const [update, setUpdate] = useState(false);
   const [state, setState] = useState(false);
 
   useEffect(() => {
@@ -14,51 +12,48 @@ const App = () => {
       .then(res => res.json())
       .then(
         result => {
-          setPetsData(result);
+          if (!state) {
+            setPetsData(result);
+          }
         },
         error => {
           console.warn('Deu erro');
         },
       );
-  }, []);
+  }, [update, state]);
 
-  const ordernarId = () => {
-    let order = petsData.sort(function (a, b) {
-      return a.id < b.id ? -1 : a.id > b.id ? 1 : 0;
-    });
-    setOrderId(order);
-    setState('id');
+  const ordernar = ordem => {
+    let order;
+    if (ordem === 'id') {
+      order = petsData.sort((a, b) => {
+        return a.id < b.id ? -1 : a.id > b.id ? 1 : 0;
+      });
+      setPetsData(order);
+      setUpdate(!update);
+      setState(true);
+    } else if (ordem === 'price') {
+      order = petsData.sort((a, b) => {
+        return a.price < b.price ? -1 : a.price > b.price ? 1 : 0;
+      });
+      setPetsData(order);
+      setUpdate(!update);
+      setState(true);
+    } else {
+      order = petsData.sort((a, b) => {
+        return a.type < b.type ? -1 : a.type > b.type ? 1 : 0;
+      });
+      setPetsData(order);
+      setUpdate(!update);
+      setState(true);
+    }
   };
 
-  const ordernarType = () => {
-    let order = petsData.sort(function (a, b) {
-      return a.type < b.type ? -1 : a.type > b.type ? 1 : 0;
-    });
-    setOrderType(order);
-    setState('type');
-  };
-
-  const ordernarPrice = () => {
-    let order = petsData.sort(function (a, b) {
-      return a.price < b.price ? -1 : a.price > b.price ? 1 : 0;
-    });
-    setOrderPrice(order);
-    setState('price');
-  };
   return (
     <View style={styles.container}>
       <Text style={styles.txtTop}>{'> Ordenação >'}</Text>
       <View style={styles.containerTop}>
         {petsData ? (
-          state === 'id' ? (
-            orderId?.map(item => <Pets {...item} key={String(item.id)} />)
-          ) : state === 'type' ? (
-            orderType?.map(item => <Pets {...item} key={String(item.id)} />)
-          ) : state === 'price' ? (
-            orderPrice?.map(item => <Pets {...item} key={String(item.id)} />)
-          ) : (
-            petsData?.map(item => <Pets {...item} key={String(item.id)} />)
-          )
+          petsData.map(item => <Pets {...item} key={String(item.id)} />)
         ) : (
           <ActivityIndicator color="#fff" size={30} />
         )}
@@ -67,20 +62,27 @@ const App = () => {
         <View style={styles.containerBottom}>
           <Text style={styles.txtBottom}>Ordenar por:</Text>
           <View style={styles.width}>
-            <Button title="id" onPress={ordernarId} color="red" />
+            <Button title="id" onPress={() => ordernar('id')} color="red" />
           </View>
           <View style={styles.width}>
-            <Button title="type" onPress={ordernarType} color="green" />
+            <Button
+              title="type"
+              onPress={() => ordernar('type')}
+              color="green"
+            />
           </View>
           <View style={styles.width}>
-            <Button title="price" onPress={ordernarPrice} color="blue" />
+            <Button
+              title="price"
+              onPress={() => ordernar('price')}
+              color="blue"
+            />
           </View>
         </View>
       </View>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
